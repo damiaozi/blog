ad326b06aadab5ef81ee2677d7d64eac00274155
wuyuanmiao <2453258306@qq.com>
2017-04-26 20:33:52 +0800

    完成了博客列表页分页获取数据，博客详情页数据获取的功能。

--- index.js
+++ index.js
@@ -9,18 +9,20 @@ const port= 9999;
 const mainUrl = 'static/index.html';
 const server = http.createServer((req,res)=>{
 	let theUrl = url.parse(req.url);
-	console.log('theUrl',theUrl);
+	// console.log('theUrl',theUrl);
 	let filePath = path.join(__dirname,'/static/',theUrl.path);
-
-	if(theUrl.query == null){
+	filePath = filePath.split('?')[0];
+	if(theUrl.query==null || theUrl.path.indexOf('/api')<0){
+	
 		//无参数,返回html页面
 		if (theUrl.path =='/'){
 			filePath = path.join(__dirname,'static/index.html');
 		}
+		// console.log('filePath',filePath);
 		let filelas = '.'+filePath.match(/\.(\w+)$/)[1];
 		// console.log('filelas',filelas);
 		if (routerMime[filelas]) {
-			console.log('routerMime',routerMime[filelas]);
+			// console.log('routerMime',routerMime[filelas]);
 			res.writeHead(200, {
 			            "Content-Type": routerMime[filelas]
 			});
@@ -36,11 +38,12 @@ const server = http.createServer((req,res)=>{
 	{
 		//有参数,处理ajax请求
 		let getReq = querystring.parse(theUrl.query);
-		console.log('getReq',getReq);
+		// console.log('getReq',getReq);
 		let json = {};
 		//判断是什么接口
 		
 		let MongoClient = require('mongodb').MongoClient;
+		var ObjectID = require('mongodb').ObjectID
 		let dbUrl = 'mongodb://127.0.0.1:27017/blog';
 		MongoClient.connect(dbUrl,(err,db)=>{
 			if(err){
@@ -54,18 +57,19 @@ const server = http.createServer((req,res)=>{
 					//查看主页的访客数据
 					let cAppInfo = db.collection('app_info');
 					cAppInfo.find({}).toArray((err,data)=>{
-						console.log('res-end',data);
+						// console.log('res-end',data);
 						// console.log('callback-data',data);
 						let json = JSON.stringify(data[0]);
-						console.log('json',json);
+						// console.log('json',json);
 					 	res.writeHead(200, {});
 						res.end(json);
+						db.close();
 					});
 				}
 			}
 
 			//设置访客数接口
-			if (req.url.indexOf('setvisit')>=0) {
+			else if (req.url.indexOf('setvisit')>=0) {
 				if(getReq['main']){
 					//查看主页的访客数据
 					let cAppInfo = db.collection('app_info');
@@ -91,21 +95,88 @@ const server = http.createServer((req,res)=>{
 					        	res.writeHead(200, {});
 								res.end('ok');
 					        }
-						});
-
-					 	
+					        db.close();
+						});	
 					});
 				}
 			}
 		
-			
-        });
-				
-		
+			//获取文章详情接口
+			else if (req.url.indexOf('blogdetail')>=0){
+				if(getReq['blogid']){
+					//传了blogid这个字段
+					let cBlogTb = db.collection('blog_tb');
+						// console.log('cBlogTb',cBlogTb);
+						findOneData(cBlogTb,{_id:ObjectID(getReq['blogid'])},(data)=>{
+							 // console.log('findOneData',data);
+							let json = JSON.stringify(data);
+							// console.log('findOneJson',json);
+							res.writeHead(200, {});
+							res.end(json);
+							db.close();
+						});
+
+					function findOneData(collection,qurery,callback){
+						collection.findOne(qurery,(err,r)=>{
+
+							if (typeof callback =='function') {
+								callback(r);
+							}
+						});
+					}
+				}else{
+					//404  没有这篇文章
+				}
+			}
+
+			//获取文章列表接口
+			else if (req.url.indexOf('bloglist')>=0){
+				if(getReq['page']){
+					let nPage = parseInt(getReq['page']);
+					// console.log('nPage',nPage);
+					// console.log('nPage',typeof nPage);
+					// console.log('ppage',parseInt(nPage));
+					let nRows = parseInt(getReq['rows']);
+					// console.log('nRows',nRows);
+
+					let nSkip = (nPage-1)*nRows;
+					// console.log('nSkip',nSkip);
+					//传了blogid这个字段
+					let cBlogTb = db.collection('blog_tb');
+					cBlogTb.find({}).skip(nSkip).limit(nRows).toArray((err,data)=>{
+						// console.log('findData',data);
+						let json = JSON.stringify(data);
+						
+						//计算总共有多少条数据，返回页码给total
+						cBlogTb.count((err,count)=>{
+							let total = Math.ceil(count/10);
+							json = `{"total":${total},"data":${json}}`;
+						
+							// console.log('findJson',json);
+							res.writeHead(200, {});
+							res.end(json);
+							db.close();
+						});
+						
+					});
+
+					// function findData(collection,query,limit,callback){
+					// 	collection.find(query,limit).toArray((err,r)=>{
+
+					// 		if (typeof callback =='function') {
+					// 			callback(r);
+					// 		}
+					// 	});
+					// }
+				}else{
+					//404  没有这篇文章
+				}
+			}
+
+
+
+        });		
 	}
-
-	
-
 });
 
 server.listen(port,hostname,()=>{
--- static/js/blog.js
+++ static/js/blog.js
@@ -9,3 +9,76 @@ var showBlogType = function(){
 };
 
 $('#blog-search').click(showBlogType);
+
+
+// 请求服务器，获取当前博客列表数据
+var sHostUrl = '';
+var sUrlBloglst = sHostUrl+'/api/bloglist'
+var oBlogLst = $('#blog-list');
+
+//分页的ul标签
+var oPageLst = $('#j-page-list');
+
+getBlogLst(1);
+function getBlogLst(page){
+	$.ajax({
+		type:'get',
+		url:sUrlBloglst,
+		data:{'page':page,'rows':10},
+		dataType:'json',
+		success:function(json){
+			// console.log('blog_list',json);
+			var total = json['total'];
+			var data = json['data'];
+			oBlogLst.children().remove();
+			//创建li标签，并且把数据填上，再添加到ul标签里
+			for(var i in data){
+				var json = data[i];
+				var _id = json._id;
+				var sLiHtml = `<li class="blog-item"><div class="blog-content">
+							<a href="blog_detail.html?blogid=${_id}"><h2>${json.tittle}</h2></a>
+							<div class="blog-tag"><i></i><a href="Javascript:;">${json.typename}</a></div>
+							<a href="blog_detail.html?blogid=${_id}"><p class="blog-review">${json.brief}</p></a>
+							<div class="blog-info">
+								<div class="blog-time">
+									<i></i>
+									<span>${json.createtime}</span>
+								</div>
+								<div class="blog-read">
+									<i>阅</i>
+									<span>${json.readnum}</span>
+									<i>评</i>
+									<span>${json.comtnum}</span>
+									<i>赞</i>
+									<span>${json.colnum}</span>
+							</div></div></div></li>`;
+				$(sLiHtml).appendTo(oBlogLst);
+			}	
+			//初始化之后就不重复执行了
+			if (!bPageInit) {
+				pageInit(total);
+			}
+		
+			
+		}
+	});
+}
+var bPageInit = false;
+//初始化分页的数据
+function pageInit(total){
+	bPageInit = true;
+	//分页选项设置total个
+	for (var i = 0; i < total; i++) {
+		var sLi = `<li><a href="#">${i+1}</a></li>`;
+		$(sLi).appendTo(oPageLst);
+	}
+
+	var oPageLi = $('#j-page-list li');
+	oPageLi.click(function(){
+		var nPage = $(this).index()+1;
+		// console.log('oPageLi.click',nPage);
+		getBlogLst(nPage);
+	});
+}
+
+
